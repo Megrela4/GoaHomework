4f3ae609209b5919bf6dbddc53cd8d80b59bb4ac
Megrela4 <nikol.megrelishvili@gmail.com>
2025-05-02 17:26:48 +0400

    Add files via upload

---
+++ level 68/cw/script.js
@@ -0,0 +1,55 @@
+
+// 1) ახსენით რა არის DOM'ი - არის მოდელი რომელიც აღწერს ვებ-გვერდის სტრუქტურას ისე რომ პროგრამებს მაგალითად JavaScript-ს შეეძლოს იმ ვებ-გვერდთან ურთიერთობა
+
+
+
+
+
+
+// 3) შეასრულეთ მინიმუმ 3-3 მაგალითი თითოეულ მათემატიკურ ოპერაციაზე და გამოიტანეთ კონსოლში 
+console.log("Addition Examples:");
+console.log(5 + 3);     
+console.log(10 + 20);   
+console.log(-4 + 7);    
+
+
+console.log("Subtraction Examples:");
+console.log(9 - 2);     
+console.log(100 - 50);  
+console.log(3 - 10);    
+
+
+console.log("Multiplication Examples:");
+console.log(4 * 2);     
+console.log(7 * -1);    
+
+
+console.log("Division Examples:");
+console.log(10 / 2);    
+console.log(9 / 3);    
+console.log(5 / 2);     
+
+
+console.log("Modulus Examples:");
+console.log(10 % 3);    
+console.log(9 % 4);     
+console.log(8 % 2);     
+
+
+console.log("Exponentiation Examples:");
+console.log(2 ** 3);    
+console.log(5 ** 2);    
+console.log(3 ** 0);    
+
+
+
+// 4) HTML'ში შექმენით პარაგრაფი სადაც ჩაწერთ თქვენთვის სასურველ ტექსტს, შემდეგ ეს ტქესტი გადმოიტანეთ JavaScript'ში და შეინახეთ ცვლადშ ბოლოს კი გამოიტანეთ ეს ცვლადი 
+let paragraphContent = document.getElementById("myText").textContent;
+console.log(paragraphContent);
+
+
+
+
+
+
+
